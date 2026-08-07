5ec07f58a213eb4e0e2e58ee493e28185993f7eb
Abhishek94736 <anilabhishek75@gmail.com>
2026-08-07 09:42:16 +0530

    Update promp.js

--- assets/js/promp.js
+++ assets/js/promp.js
@@ -8,9 +8,16 @@ if(navToggle){
     })
 }
 
-
 if(navClose){
      navClose.addEventListener('click', () =>{
          navMenu.classList.remove('show-menu')
      })
 }
+
+const navLink = document.querySelectorAll('.nav_link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
