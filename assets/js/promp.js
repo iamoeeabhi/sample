c4e7e8e1ceaba6c31c99a3cb33768c070f25dc73
Abhishek Patel <anilabhishek75@gmail.com>
2026-08-22 21:12:57 +0530

    Update promp.js

--- assets/js/promp.js
+++ assets/js/promp.js
@@ -129,3 +129,13 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >=80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll');else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
